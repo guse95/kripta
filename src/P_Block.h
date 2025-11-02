61fed81439fa5df69473fa4d50c3fc6964de4400
guse95 <saveliy.gusev.05@list.ru>
2025-11-02 21:29:10 +0300

    P_block v2

---
+++ src/P_Block.h
@@ -0,0 +1,15 @@
+#ifndef P_BLOCK_H
+#define P_BLOCK_H
+
+#include <iostream>
+#include <cstdint>
+
+uint8_t get_bit(const uint8_t* text, size_t ind, size_t size_text, bool is_indexing_strait);
+
+void set_bit(uint8_t* text, size_t new_ind, uint8_t bit, size_t size_text, bool is_indexing_strait);
+
+void permutations(const uint8_t* block, const size_t size_block,
+    const int* p_block, const size_t size_p, uint8_t* new_block,
+    bool is_indexing_strait); // strait == 63 -> 0
+
+#endif
