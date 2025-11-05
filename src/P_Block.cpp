8d22e177c673b19b137f5b479e4fd8dfb89aeaf6
guse95 <saveliy.gusev.05@list.ru>
2025-11-05 15:35:07 +0300

    P_Block v5

--- src/P_Block.cpp
+++ src/P_Block.cpp
@@ -1,31 +1,31 @@
 #include "P_Block.h"
 
-uint8_t get_bit(const uint8_t* text, const size_t ind, const size_t size_text, bool is_indexing_strait)
+uint8_t get_bit(const uint8_t* text, const size_t ind, const size_t size_text, ByteOrder order)
 {
     uint8_t bit;
-    if (is_indexing_strait)
+    if (order == ByteOrder::LittleEndian) // little endian
         bit = text[(size_text - 1 - ind) / 8] & (1 << (ind % 8));
     else
-        bit = text[ind / 8] & (1 << ((size_text - 1 - ind) % 8));
+        bit = text[ind / 8] & (1 << (ind % 8));
     return bit > 0;
 }
 
-void set_bit(uint8_t* text, const size_t new_ind, uint8_t bit, const size_t size_text, bool is_indexing_strait)
+void set_bit(uint8_t* text, const size_t new_ind, uint8_t bit, const size_t size_text, ByteOrder order)
 {
-    if (is_indexing_strait)
-        text[(size_text - 1 - new_ind + 7) / 8] |= bit << (new_ind % 8);
+    if (order == ByteOrder::LittleEndian)
+        text[(size_text - 1 - new_ind) / 8] |= bit << (new_ind % 8);
     else
-        text[new_ind / 8] |= bit << ((size_text - 1 - new_ind) % 8);
+        text[new_ind / 8] |= bit << (new_ind % 8);
 }
 
 void permutations(const uint8_t* block, const size_t size_block,
     const int* p_block, const size_t size_p, uint8_t* new_block,
-    bool is_indexing_strait, bool is_indexing_from_zero) // strait == 63 -> 0
+    const ByteOrder order, bool is_indexing_from_zero) // strait == 63 -> 0
 {
     for (size_t i = 0; i < size_p; i++)
     {
-        const uint8_t bit = get_bit(block, p_block[i] - (is_indexing_from_zero? 1:0),
-            size_block, is_indexing_strait);
-        set_bit(new_block, i, bit, size_p, is_indexing_strait);
+        const uint8_t bit = get_bit(block, p_block[i] - (is_indexing_from_zero? 0:1),
+            size_block, order);
+        set_bit(new_block, i, bit, size_p, order);
     }
 }
--- src/P_Block.h
+++ src/P_Block.h
@@ -4,12 +4,14 @@
 #include <iostream>
 #include <cstdint>
 
-uint8_t get_bit(const uint8_t* text, size_t ind, size_t size_text, bool is_indexing_strait);
+enum class ByteOrder{BigEndian, LittleEndian};
 
-void set_bit(uint8_t* text, size_t new_ind, uint8_t bit, size_t size_text, bool is_indexing_strait);
+uint8_t get_bit(const uint8_t* text, size_t ind, size_t size_text, const ByteOrder order);
+
+void set_bit(uint8_t* text, size_t new_ind, uint8_t bit, size_t size_text, const ByteOrder order);
 
 void permutations(const uint8_t* block, const size_t size_block,
     const int* p_block, const size_t size_p, uint8_t* new_block,
-    bool is_indexing_strait, bool is_indexing_from_zero); // strait == 63 -> 0
+    const ByteOrder order = ByteOrder::BigEndian, bool is_indexing_from_zero = false); // strait == 63 -> 0
 
 #endif
