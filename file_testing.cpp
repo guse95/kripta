fa0cc2b8b4a7423f4430e37c8191452e0eb24812
guse95 <saveliy.gusev.05@list.ru>
2025-12-03 17:21:17 +0300

    Tests for DES & DEAL

--- file_testing.cpp
+++ file_testing.cpp
@@ -8,24 +8,59 @@
 int main()
 {
     // uint8_t key[8] = {0, 124, 2, 0, 12, 0, 0, 132};
-    uint8_t key[8] = {10, 23, 54, 3, 124, 43, 76, 255};
-    uint8_t key2[16] = {10, 23, 54, 3, 124, 43, 76, 255, 10, 23, 54, 3, 124, 43, 76, 255};
-    uint8_t iv[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    uint8_t key_des[8] = {'a', 's', 'd', 'f', 'e', 'd', 'c', 'b'};
+    uint8_t key_deal_128[16] = {'z', 'x', 'c', 'v', 'b', 'n', 'm',
+        ',', '.', '/', 'a', 's', 'd', 'f', 'g', 'h'};
+    uint8_t key_deal_192[24] = {'j', 'u', 'g', '1', '2', 'n', 'a',
+        's', 'd', 'f', 'e', 'd', 'c', 'b', 'm', ',',
+        '.', '/', 'a', 's', 'd', 'f', 'g', 'h'};
+    uint8_t key_deal_256[32] = {'p', 'o', 'i', 'u', '9', '8', '7',
+        '6', '5', '4', 'a', 's', 'd', 'f', 'g', 'h',
+        'z', 'x', 'c', 'v', 'b', 'n', 'm', 's', 'd',
+        'k', 'l', 'h', 'n', 'm', 'g', 'h'};
 
-    auto alg = new DES();
-    auto alg2 = new DEAL(128);
+    uint8_t iv_des[8] = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i'};
+    uint8_t iv_deal[16] = {'1', '2', '3', '4', '5', '6', '7', '8',
+        '9', 'g', 'f', 'e', 'd', 'c', 'b', 'a'};
 
-    CipherContext Cont(alg2, key2, Mode::ECB, Padding::ZEROS, 16, iv, {8});
+    auto alg_des = DES();
+    auto alg_deal_128 = DEAL(128);
+    auto alg_deal_192 = DEAL(192);
+    auto alg_deal_256 = DEAL(256);
+
+    // CipherContext Cont(&alg_deal_192, key_deal_192, Mode::ECB, Padding::PKCS7, 16, iv_deal, {8});
 
     // Cont.encrypt("vid.mp4", "encrypted.txt");
     // Cont.decrypt("encrypted.txt", "decrypted.mp4");
-    //
-    // Cont.encrypt("img.png", "encrypted.txt");
+
+    // Cont.encrypt("33.png", "encrypted.txt");
     // Cont.decrypt("encrypted.txt", "decrypted.png");
     //
-    Cont.encrypt("text.txt", "encrypted.txt");
-    Cont.decrypt("encrypted.txt", "decrypted.txt");
+    // Cont.encrypt("text.txt", "encrypted.txt");
+    // Cont.decrypt("encrypted.txt", "decrypted.txt");
 
-    delete alg;
-    delete alg2;
+    ISymmetricCypher* algs[] = {&alg_des, &alg_deal_128, &alg_deal_192, &alg_deal_256};
+    uint8_t* keys[] = {key_des, key_deal_128, key_deal_192, key_deal_256};
+    uint8_t* ivs[] = {iv_des, iv_deal, iv_deal, iv_deal};
+    constexpr Mode modes[] = {Mode::ECB, Mode::CBC, Mode::PCBC, Mode::CFB, Mode::OFB, Mode::CTR, Mode::RandomDelta };
+    constexpr Padding paddings[] = {Padding::ZEROS, Padding::PKCS7, Padding::ANSI_X923, Padding::ISO10126};
+    uint64_t block_sz[] = {8, 16, 16, 16};
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 7; j++)
+        {
+            for (int k = 0; k < 4; k++)
+            {
+                CipherContext Cont(algs[i], keys[i], modes[j], paddings[k], block_sz[i], ivs[i], {8});
+
+                std::string ind = std::to_string(i) + "_" + std::to_string(j) + "_" + std::to_string(k);
+                std::string encr_file_name = "EncrRes/encr_" + ind + ".txt";
+                std::string decr_file_name = "DecrRes/decr_" + ind + ".png";
+
+                Cont.encrypt("33.png", encr_file_name);
+                Cont.decrypt(encr_file_name, decr_file_name);
+            }
+        }
+    }
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-// #include "CipherContext.h"
+#include "CipherContext.h"
 #include "DES.h"
 #include "DEAL.h"
 
@@ -9,67 +9,67 @@ int main()
 {
     // uint8_t text[] = "Some text to check DES.\nSome more text.";
     // uint8_t key[8] = {0, 124, 2, 0, 12, 0, 0, 132};
-    // uint8_t text[] = "Some text.txt to check if DES works.\nIf you see this, I half won!";
-    // uint8_t key[8] = {10, 23, 54, 3, 124, 43, 76, 255};
-    // uint8_t iv[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    uint8_t text[] = "Some text to check if DES works.\nIf you see this, I half won!";
+    uint8_t key[8] = {10, 23, 54, 3, 124, 43, 76, 255};
+    uint8_t iv[8] = {1, 2, 3, 4, 5, 6, 7, 8};
 
-    uint8_t key2[16] = {10, 23, 54, 3, 124, 43, 76, 255, 0, 1, 2, 3, 4, 5, 6, 7};
+    uint8_t iv2[16] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', 'g', 'f', 'e', 'd', 'c', 'b', 'a'};
+    uint8_t key2[16] = {'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'a', 's', 'd', 'f', 'g', 'h'};
 
-    // const auto alg = new DES();
+    const auto alg = new DES();
     const auto alg2 = new DEAL(128);
-    // const CipherContext Cont(alg2, key2, Mode::ECB, Padding::ZEROS, 16, iv, {2});
+    const CipherContext Cont(alg2, key2, Mode::RandomDelta, Padding::ZEROS, 16, iv2, {2});
 
-    uint8_t text_deal[16] = "Some text to ch";
-    uint8_t encr_text[16] = {0};
-    uint8_t decr_text[16] = {0};
-
-    alg2->encrypt(text_deal, encr_text, key2);
-
-    std::cout << "Encrypted text: " << std::endl;
-    for (int i = 0; i < 16; i++)
-    {
-        std::cout << encr_text[i] << " ";
-    }
-    std::cout << std::endl;
-
-    alg2->decrypt(encr_text, decr_text, key2);
-
-    std::cout << "Decrypted text: " << std::endl;
-    for (uint64_t i = 0; i < 16; i++)
-    {
-        std::cout << decr_text[i];
-        // printf("%c", decrtext[i]);
-    }
-    std::cout << std::endl;
-
-
-    // uint64_t encr_sz;
-    // uint8_t* encrtext = Cont.encrypt(text, sizeof(text) / sizeof(uint8_t), encr_sz);
+    // uint8_t text_deal[16] = "Some text to ch";
+    // uint8_t encr_text[16] = {0};
+    // uint8_t decr_text[16] = {0};
     //
-    // if (encrtext == nullptr)
-    // {
-    //     std::cerr << "Encrypt failed" << std::endl;
-    // }
+    // alg2->encrypt(text_deal, encr_text, key2);
+    //
     // std::cout << "Encrypted text: " << std::endl;
-    // for (int i = 0; i < encr_sz; i++)
+    // for (int i = 0; i < 16; i++)
     // {
-    //     std::cout << encrtext[i] << " ";
+    //     std::cout << encr_text[i] << " ";
     // }
     // std::cout << std::endl;
     //
-    // uint64_t decr_sz;
-    // uint8_t* decrtext = Cont.decrypt(encrtext, encr_sz, decr_sz);
+    // alg2->decrypt(encr_text, decr_text, key2);
     //
     // std::cout << "Decrypted text: " << std::endl;
-    // for (uint64_t i = 0; i < decr_sz - 1; i++)
+    // for (uint64_t i = 0; i < 16; i++)
     // {
-    //     std::cout << decrtext[i];
+    //     std::cout << decr_text[i];
     //     // printf("%c", decrtext[i]);
     // }
     // std::cout << std::endl;
 
-    // delete alg;
+
+    uint64_t encr_sz;
+    uint8_t* encrtext = Cont.encrypt(text, sizeof(text) / sizeof(uint8_t), encr_sz);
+
+    if (encrtext == nullptr)
+    {
+        std::cerr << "Encrypt failed" << std::endl;
+    }
+    std::cout << "Encrypted text: " << std::endl;
+    for (int i = 0; i < encr_sz; i++)
+    {
+        std::cout << encrtext[i] << " ";
+    }
+    std::cout << std::endl;
+
+    uint64_t decr_sz;
+    uint8_t* decrtext = Cont.decrypt(encrtext, encr_sz, decr_sz);
+
+    std::cout << "Decrypted text: " << std::endl;
+    for (uint64_t i = 0; i < decr_sz - 1; i++)
+    {
+        std::cout << decrtext[i];
+    }
+    std::cout << std::endl;
+
+    delete alg;
     delete alg2;
-    // delete[] encrtext;
-    // delete[] decrtext;
+    delete[] encrtext;
+    delete[] decrtext;
 }
