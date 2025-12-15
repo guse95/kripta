0a81d6a26674caa417fddafe11917b2682b96413
guse95 <saveliy.gusev.05@list.ru>
2025-12-15 23:14:20 +0300

    PrimeTestBase v2 (BigInt -> mpz_class)

--- src/RSA/IPrimeTest.h
+++ src/RSA/IPrimeTest.h
@@ -1,12 +1,12 @@
 #ifndef IPRIMETEST_H
 #define IPRIMETEST_H
-#include "my_BigInt.h"
+#include <gmpxx.h>
 
 
 class IPrimeTest {
     public:
     virtual ~IPrimeTest() = default;
 
-    [[nodiscard]] virtual bool isPrime(const BigInt& num, double min_probability) const = 0;
+    [[nodiscard]] virtual bool isPrime(const mpz_class& num, double min_probability) const = 0;
 };
 #endif
--- src/RSA/PrimeTestBase.h
+++ src/RSA/PrimeTestBase.h
@@ -1,5 +1,7 @@
 #ifndef PRIMETESTBASE_H
 #define PRIMETESTBASE_H
+#include <chrono>
+
 #include "IPrimeTest.h"
 #include "ServiceGCD.h"
 
@@ -8,22 +10,26 @@ class PrimeTestBase : public IPrimeTest {
     public:
     ~PrimeTestBase() override = default;
 
-    [[nodiscard]] virtual bool testIteration(const BigInt& a, const BigInt& n) const = 0;
+    [[nodiscard]] virtual bool testIteration(const mpz_class& a, const mpz_class& n) const = 0;
 
     [[nodiscard]] virtual double getProbForOneIter() const = 0;
 
-    [[nodiscard]] bool isPrime(const BigInt& num, const double min_probability) const override
+    [[nodiscard]] bool isPrime(const mpz_class& num, const double min_probability) const override
     {
         if (min_probability < 0.5 || min_probability >= 1.0) {
             throw std::invalid_argument("min_probability must be in [0.5, 1)");
         }
         const double prob_for_one_iter = getProbForOneIter();
         double tmp_prob = 1.0;
+
+        gmp_randclass rng(gmp_randinit_default);
+        rng.seed(std::chrono::system_clock::now().time_since_epoch().count());
+
         while ( (1 - tmp_prob) < min_probability)
         {
             tmp_prob *= prob_for_one_iter;
-            BigInt a = BigInt::rnd(num);
-            // if (ServiceGCD::gcd(a, num) != BigInt(1))
+            mpz_class a = rng.get_z_range(num - 1);
+            // if (ServiceGCD::gcd(a, num) != mpz_class(1))
             // {
             //     return false;
             // }
