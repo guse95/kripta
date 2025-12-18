67f0f80c1f337d0d6112db1fa1f7873dea60ff6d
guse95 <saveliy.gusev.05@list.ru>
2025-12-18 21:54:50 +0300

    test for lab 3

--- file_testing.cpp
+++ file_testing.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "CipherContext.h"
+#include "AES/AES.h"
 #include "DES/DES.h"
 #include "DEAL/DEAL.h"
 
@@ -9,26 +10,57 @@ int main()
 {
     // uint8_t key[8] = {0, 124, 2, 0, 12, 0, 0, 132};
     uint8_t key_des[8] = {'a', 's', 'd', 'f', 'e', 'd', 'c', 'b'};
-    uint8_t key_deal_128[16] = {'z', 'x', 'c', 'v', 'b', 'n', 'm',
-        ',', '.', '/', 'a', 's', 'd', 'f', 'g', 'h'};
-    uint8_t key_deal_192[24] = {'j', 'u', 'g', '1', '2', 'n', 'a',
-        's', 'd', 'f', 'e', 'd', 'c', 'b', 'm', ',',
-        '.', '/', 'a', 's', 'd', 'f', 'g', 'h'};
-    uint8_t key_deal_256[32] = {'p', 'o', 'i', 'u', '9', '8', '7',
-        '6', '5', '4', 'a', 's', 'd', 'f', 'g', 'h',
-        'z', 'x', 'c', 'v', 'b', 'n', 'm', 's', 'd',
-        'k', 'l', 'h', 'n', 'm', 'g', 'h'};
+    uint8_t key_deal_128[16] = {
+        'z', 'x', 'c', 'v', 'b', 'n', 'm', ',',
+        '.', '/', 'a', 's', 'd', 'f', 'g', 'h'
+    };
+    uint8_t key_deal_192[24] = {
+        'j', 'u', 'g', '1', '2', 'n', 'a', 's',
+        'd', 'f', 'e', 'd', 'c', 'b', 'm', ',',
+        '.', '/', 'a', 's', 'd', 'f', 'g', 'h'
+    };
+    uint8_t key_deal_256[32] = {
+        'p', 'o', 'i', 'u', '9', '8', '7', '6',
+        '5', '4', 'a', 's', 'd', 'f', 'g', 'h',
+        'z', 'x', 'c', 'v', 'b', 'n', 'm', 's',
+        'd', 'k', 'l', 'h', 'n', 'm', 'g', 'h'
+    };
 
     uint8_t iv_des[8] = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i'};
-    uint8_t iv_deal[16] = {'1', '2', '3', '4', '5', '6', '7', '8',
-        '9', 'g', 'f', 'e', 'd', 'c', 'b', 'a'};
+    uint8_t iv_deal[16] = {
+        '1', '2', '3', '4', '5', '6', '7', '8',
+        '9', 'g', 'f', 'e', 'd', 'c', 'b', 'a'
+    };
+    uint8_t iv_aes_192[24] = {
+        'l', 'k', 'j', 'h', 'g', 'v', 'x', 'p',
+        'f', 'f', 'f', 'f', 'a', 'b', 'm', 'd',
+        '.', '/', 'a', 's', 'Z', 'Z', 'Z', 'Z'
+    };
+    uint8_t iv_aes_256[32] = {
+        '0', '9', '8', '7', '6', '5', '4', '3',
+        '2', '1', '1', '1', '1', 'f', 'g', 'h',
+        'z', 'x', 'c', 'v', 'b', 'n', 'm', 's',
+        'd', 'k', 'l', 'h', '1', '1', '1', '1'
+    };
 
     auto alg_des = DES();
     auto alg_deal_128 = DEAL(128);
     auto alg_deal_192 = DEAL(192);
     auto alg_deal_256 = DEAL(256);
 
-    // CipherContext Cont(&alg_deal_192, key_deal_192, Mode::ECB, Padding::PKCS7, 16, iv_deal, {8});
+    auto alg_aes_128_128 = AES(16, 16, key_deal_128);
+    auto alg_aes_128_192 = AES(16, 24, key_deal_192);
+    auto alg_aes_128_256 = AES(16, 32, key_deal_256);
+
+    auto alg_aes_192_128 = AES(24, 16, key_deal_128);
+    auto alg_aes_192_192 = AES(24, 24, key_deal_192);
+    auto alg_aes_192_256 = AES(24, 32, key_deal_256);
+
+    auto alg_aes_256_128 = AES(32, 16, key_deal_128);
+    auto alg_aes_256_192 = AES(32, 24, key_deal_192);
+    auto alg_aes_256_256 = AES(32, 32, key_deal_256);
+
+    // CipherContext Cont(&alg_aes_192_192, key_deal_192, Mode::ECB, Padding::PKCS7, 24, iv_aes_192, {8});
 
     // Cont.encrypt("vid.mp4", "encrypted.txt");
     // Cont.decrypt("encrypted.txt", "decrypted.mp4");
@@ -39,22 +71,33 @@ int main()
     // Cont.encrypt("text.txt", "encrypted.txt");
     // Cont.decrypt("encrypted.txt", "decrypted.txt");
 
-    ISymmetricCypher* algs[] = {&alg_des, &alg_deal_128, &alg_deal_192, &alg_deal_256};
-    uint8_t* keys[] = {key_des, key_deal_128, key_deal_192, key_deal_256};
-    uint8_t* ivs[] = {iv_des, iv_deal, iv_deal, iv_deal};
+    // ISymmetricCypher* algs[] = {&alg_des, &alg_deal_128, &alg_deal_192, &alg_deal_256};
+    // uint8_t* keys[] = {key_des, key_deal_128, key_deal_192, key_deal_256};
+    // uint8_t* ivs[] = {iv_des, iv_deal, iv_deal, iv_deal};
+
+
+    ISymmetricCypher* algs[] = {
+        &alg_aes_128_128, &alg_aes_128_192, &alg_aes_128_256,
+        &alg_aes_192_128, &alg_aes_192_192, &alg_aes_192_256,
+        &alg_aes_256_128, &alg_aes_256_192, &alg_aes_256_256,
+    };
+    uint8_t* keys[] = {key_deal_128, key_deal_192, key_deal_256};
+    uint8_t* ivs[] = {iv_deal, iv_aes_192, iv_aes_256};
+
     constexpr Mode modes[] = {Mode::ECB, Mode::CBC, Mode::PCBC, Mode::CFB, Mode::OFB, Mode::CTR, Mode::RandomDelta };
     constexpr Padding paddings[] = {Padding::ZEROS, Padding::PKCS7, Padding::ANSI_X923, Padding::ISO10126};
-    uint64_t block_sz[] = {8, 16, 16, 16};
+    uint64_t block_sz[] = {16, 24, 32};
 
-    for (int i = 0; i < 4; i++)
+
+    for (int iv = 0; iv < 3; iv++)
     {
-        for (int j = 0; j < 7; j++)
+        for (int k = 0; k < 3; k++)
         {
-            for (int k = 0; k < 4; k++)
+            for (int j = 0; j < 7; j++)
             {
-                CipherContext Cont(algs[i], keys[i], modes[j], paddings[k], block_sz[i], ivs[i], {8});
+                CipherContext Cont(algs[iv * 3 + k], keys[k], modes[j], Padding::ZEROS, block_sz[iv], ivs[iv], {8});
 
-                std::string ind = std::to_string(i) + "_" + std::to_string(j) + "_" + std::to_string(k);
+                std::string ind = std::to_string(iv) + "_" + std::to_string(k) + "_" + std::to_string(j);
                 std::string encr_file_name = "EncrRes/encr_" + ind + ".txt";
                 std::string decr_file_name = "DecrRes/decr_" + ind + ".png";
 
--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 #include <gmpxx.h>
 
 #include "WienerAttack.h"
+#include "AES/AES.h"
 
 
 int main()
@@ -25,15 +26,36 @@ int main()
     uint8_t iv2[16] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', 'g', 'f', 'e', 'd', 'c', 'b', 'a'};
     uint8_t key2[16] = {'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'a', 's', 'd', 'f', 'g', 'h'};
 
+
+    uint8_t iv_aes_192[24] = {
+        'n', 'm', ',', '.', '/', 'a', 's', '1',
+        '2', '3', '4', '5', '6', '7', '8', '9',
+        'g', 'f', 'e', 'd', 'c', 'b', 'a', 'o'};
+    uint8_t key_aes_192[24] = {
+        'j', 'u', 'g', '1', '2', 'n', 'a', 's',
+        'd', 'f', 'e', 'd', 'c', 'b', 'm', ',',
+        '.', '/', 'a', 's', 'd', 'f', 'g', 'h'};
+
+    uint8_t iv_aes_256[32] = {
+        '5', '6', '7', '8', '9', 'g', 'f', 'e',
+        'n', 'm', ',', '.', '/', 'a', 's', '1',
+        '2', '3', '4', '5', '6', '7', '8', '9',
+        'g', 'f', 'e', 'd', 'c', 'b', 'a', 'Z'};
+    uint8_t key_aes_256[32] = {'p', 'o', 'i', 'u', '9', '8', '7',
+        '6', '5', '4', 'a', 's', 'd', 'f', 'g', 'h',
+        'z', 'x', 'c', 'v', 'b', 'n', 'm', 's', 'd',
+        'k', 'l', 'h', 'n', 'm', 'g', 'h'};
+
     // auto alg = DES();
     // auto alg2 = DEAL(128);
-    // const CipherContext Cont(&alg2, key2, Mode::RandomDelta, Padding::ZEROS, 16, iv2, {2});
+    auto alg3 = AES(24, 16, key2);
+    const CipherContext Cont(&alg3, key2, Mode::ECB, Padding::ZEROS, 24, iv_aes_192, {2});
 
     // uint8_t text_deal[16] = "Some text to ch";
     // uint8_t encr_text[16] = {0};
     // uint8_t decr_text[16] = {0};
     //
-    // alg2->encrypt(text_deal, encr_text, key2);
+    // alg3.encrypt(text_deal, encr_text, key2);
     //
     // std::cout << "Encrypted text: " << std::endl;
     // for (int i = 0; i < 16; i++)
@@ -42,7 +64,7 @@ int main()
     // }
     // std::cout << std::endl;
     //
-    // alg2->decrypt(encr_text, decr_text, key2);
+    // alg3.decrypt(encr_text, decr_text, key2);
     //
     // std::cout << "Decrypted text: " << std::endl;
     // for (uint64_t i = 0; i < 16; i++)
@@ -53,55 +75,59 @@ int main()
     // std::cout << std::endl;
 
 
-    // uint64_t encr_sz;
-    // uint8_t* encrtext = Cont.encrypt(text, sizeof(text) / sizeof(uint8_t), encr_sz);
-    //
-    // if (encrtext == nullptr)
-    // {
-    //     std::cerr << "Encrypt failed" << std::endl;
-    // }
-    // std::cout << "Encrypted text: " << std::endl;
-    // for (int i = 0; i < encr_sz; i++)
-    // {
-    //     std::cout << encrtext[i] << " ";
-    // }
-    // std::cout << std::endl;
-    //
-    // uint64_t decr_sz;
-    // uint8_t* decrtext = Cont.decrypt(encrtext, encr_sz, decr_sz);
-    //
-    // std::cout << "Decrypted text: " << std::endl;
-    // for (uint64_t i = 0; i < decr_sz - 1; i++)
-    // {
-    //     std::cout << decrtext[i];
-    // }
-    // std::cout << std::endl;
-    //
-    // delete[] encrtext;
-    // delete[] decrtext;
+    uint64_t encr_sz;
+    uint8_t* encrtext = Cont.encrypt(text, sizeof(text) / sizeof(uint8_t), encr_sz);
+
+    if (encrtext == nullptr)
+    {
+        std::cerr << "Encrypt failed" << std::endl;
+    }
+    std::cout << "Encrypted text: " << std::endl;
+    for (int i = 0; i < encr_sz; i++)
+    {
+        std::cout << encrtext[i] << " ";
+    }
+    std::cout << std::endl;
+
+    uint64_t decr_sz;
+    uint8_t* decrtext = Cont.decrypt(encrtext, encr_sz, decr_sz);
+
+    std::cout << "Decrypted text: " << std::endl;
+    for (uint64_t i = 0; i < decr_sz - 1; i++)
+    {
+        std::cout << decrtext[i];
+    }
+    std::cout << std::endl;
+
+    delete[] encrtext;
+    delete[] decrtext;
+
+
+
+
 
     // MillerRabin s;
     // std::cout << "RESULT: " << s.isPrime(mpz_class("17"), 0.5) << std::endl;
 
 
-    RSA rsa_alg(RSA::MILLER_RABIN, 0.99, 4096);
-    rsa_alg.generateWeakKeys();
-    mpz_class mess("12345678901234567890");
-    mpz_class encr_res = rsa_alg.encrypt(mess);
-    std::cout << "Encrypted: " << encr_res << '\n';
-
-    mpz_class decr_res = rsa_alg.decrypt(encr_res);
-    std::cout << "Decrypted: " << decr_res << '\n';
-
-    WienerAttack wa;
-    // auto res = wa.chainFraction(706, 1124);
-    // for (auto i : res)
-    // {
-    //     std::cout << i << '\n';
-    // }
-
-    auto d = wa.predictKeyPriv(rsa_alg.key_pub.first, rsa_alg.key_pub.second);
-
-    std::cout << "Predicted d: " << d << '\n';
-    std::cout << "Predicted: " << ServiceGCD::mod_pow(encr_res, d, rsa_alg.key_pub.second) << '\n';
+    // RSA rsa_alg(RSA::MILLER_RABIN, 0.99, 4096);
+    // rsa_alg.generateWeakKeys();
+    // mpz_class mess("12345678901234567890");
+    // mpz_class encr_res = rsa_alg.encrypt(mess);
+    // std::cout << "Encrypted: " << encr_res << '\n';
+    //
+    // mpz_class decr_res = rsa_alg.decrypt(encr_res);
+    // std::cout << "Decrypted: " << decr_res << '\n';
+    //
+    // WienerAttack wa;
+    // // auto res = wa.chainFraction(706, 1124);
+    // // for (auto i : res)
+    // // {
+    // //     std::cout << i << '\n';
+    // // }
+    //
+    // auto d = wa.predictKeyPriv(rsa_alg.key_pub.first, rsa_alg.key_pub.second);
+    //
+    // std::cout << "Predicted d: " << d << '\n';
+    // std::cout << "Predicted: " << ServiceGCD::mod_pow(encr_res, d, rsa_alg.key_pub.second) << '\n';
 }
